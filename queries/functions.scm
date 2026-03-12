6f624b030490ce0f1b19e811dbdad8919b2bbbaf
charles-antoine fournel <charlesantoine.fournel@gmail.com>
2026-03-12 19:14:48 +0100

    add function comment definition

--- queries/functions.scm
+++ queries/functions.scm
@@ -1,2 +1,15 @@
-(function_definition
- name: (identifier) @function.name)
+;; Cas 1 : fonction avec commentaire(s) juste au-dessus
+(
+  (comment)+ @doc
+  .
+  (function_definition
+    name: (identifier) @function.name
+  ) @function.def
+)
+
+;; Cas 2 : fonction sans commentaire
+(
+  (function_definition
+    name: (identifier) @function.name
+  ) @function.def
+)
